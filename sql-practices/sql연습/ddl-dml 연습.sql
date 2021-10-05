5fecf93be9b17d9cafaf9a74ed68cdf10799f1ea
changmok <hong1071@nate.com>
2021-10-05 16:23:13 +0900

    DB 스키마

--- sql-practices/sql연습/ddl-dml 연습.sql
+++ sql-practices/sql연습/ddl-dml 연습.sql
@@ -14,5 +14,39 @@ alter table member drop juminbunho;
 desc member;
 alter table member add join_date datetime not null;
 desc member;
+alter table member add self_intro text;
+desc member;
 alter table member change deparment departments varchar(100) not null;
 desc member;
+
+-- select
+select * from member;
+
+-- insert
+insert into member(no, email, password, name, department, join_date, self_intro)
+values(null, 'hong1071@nate.com', password('1234'), '홍창목', '개발팀', now(), null);
+
+insert into member(no, email, password, name, department, join_date, self_intro)
+values(null, 'hong1071@nate.com', password('1234'), '홍지연', '개발팀', now(), null);
+
+insert into member(no, email, password, name, department, join_date, self_intro)
+values(null, 'hong1071@nate.com', password('1234'), '홍지혜', '개발팀', now(), null);
+
+-- update
+update member
+set email = 'hongchangmok@naver.com'
+where no = 1
+;
+
+-- delete
+delete from member
+where no = 3
+;
+
+-- transaction
+select @@autocommit;
+set autocommit=0;
+
+insert into member(no, email, password, name, department, join_date, self_intro)
+values(null, 'hong1071@nate.com', password('1234'), '홍창목', '개발팀', now(), null);
+
