71a707787bc0ad7e3c84b93759082666fa048cb4
changmok <hong1071@nate.com>
2021-10-05 11:24:26 +0900

    211005오전 조인 수업

--- sql-practices/sql연습/조인 문법.sql
+++ sql-practices/sql연습/조인 문법.sql
@@ -62,3 +62,64 @@ where t1.emp_no = t2.emp_no				-- 조인 조건
 	from emp t1 right join dept t2 
 		on t1.dept_no = t2.no
 	;
+    
+-- 예제 3
+-- 현재 회사 상황을 반영한 직원별 근부 부서를 사번, 이름, 근무 부서로 출력해보자.
+select t1.emp_no, concat(first_name, ' ', last_name) 'name', t3.dept_name
+from employees t1, dept_emp t2, departments t3
+where t1.emp_no = t2.emp_no
+	and t2.dept_no = t3.dept_no
+	and t2.to_date = '9999-01-01'
+;
+
+-- 예제 4
+-- 현재 회사에서 지급되고 있는 급여체계를 반영한 결과를 출력해보자.
+-- 사번, 이흠, 연봉 형태로 출력
+select t1.emp_no, concat(first_name, ' ', last_name) 'name', t2.salary
+from employees t1, salaries t2
+where t1.emp_no = t2.emp_no
+	and t2.to_date = '9999-01-01'
+order by t2.salary desc;
+
+-- 예제 5: 현재 직책별로 평균 연봉과 인원수를 구하되 직책별로 인원이 100명 이상인 직책만 출력해보자.
+select t1.title, avg(salary), count(*)
+from titles t1, salaries t2
+where t1.emp_no = t2.emp_no
+	and t1.to_date = '9999-01-01'
+    and t2.to_date = '9999-01-01'
+group by t1.title
+having count(*) > 100
+order by avg(salary) desc
+;
+
+
+-- 예제 6: 현재 부서별로 현재 직책이 Enginerr인 직원들에 대해서만 평균 급여를 구하세요.
+select t4.dept_name, avg(salary)
+from dept_emp t1, salaries t2, titles t3, departments t4
+where t1.emp_no = t2.emp_no
+	and t2.emp_no = t3.emp_no
+    and t1.dept_no = t4.dept_no
+	and t1.to_date = '9999-01-01'
+    and t2.to_date = '9999-01-01'
+    and t3.to_date = '9999-01-01'
+    and t3.title = 'Engineer'
+group by t4.dept_name
+;
+
+-- 예제 7: 현재 직책별로 급여의 총합을 구하되 Engineer직책은 제외하세요. 
+-- 단, 총합이 2,000,000,000이상인 직책만 나타내며 급여총합에 대해서 내림차순(DESC)로 정렬하세요.   
+select t2.title, sum(t1.salary)
+from salaries t1, titles t2
+where t1.emp_no = t2.emp_no
+	and t1.to_date = '9999-01-01'
+    and t2.to_date = '9999-01-01'
+    and t2.title != 'Engineer'
+group by t2.title
+having sum(t1.salary) >= 2000000000
+order by sum(t1.salary) desc
+;
+
+
+
+
+
